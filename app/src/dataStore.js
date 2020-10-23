cde63b86a5d9008ee75899ad03f2270e2a6a08a7
Dylan Box <dylan@wedgedetroit.com>
2020-10-22 23:37:43 -0400

    Add: Preliminary Data Store
    Adds functions and tests to add, sort, and serialize category data

---
+++ app/src/dataStore.js
@@ -0,0 +1,64 @@
+export const baseCategories = []
+export const categorySets = []
+
+
+export function categorySort(a, b) {
+  // Sort in order of set length
+  const length = a.sets.length - b.sets.length
+  if (length !== 0) {
+    return length
+  }
+  // And if they're the same length,
+  // sort in order of the indexes in the sets
+  for (let index = 0; index < a.sets.length; index += 1) {
+    return a.sets[index] - b.sets[index]
+  }
+
+  return 0
+}
+
+/**
+ * Function to take our category stores and turn it into a format usable by D3
+ */
+export function serializeCategories() {
+  const serializedCategories = baseCategories.concat(categorySets)
+  serializedCategories.sort(categorySort)
+  return serializedCategories
+}
+
+/**
+ * Adds a new category to our list of categories and generates
+ * intersections for each of the existing categories
+ */
+export function addNewCategory(label, size) {
+  const newCategoryIndex = baseCategories.length
+  // Add itself to the base categories
+  const newCategory = {
+    sets: [newCategoryIndex],
+    label,
+    size,
+  }
+
+  const addToSet = (categorySet) => ({
+    // Sort the sets in place for ease of the serializer
+    sets: categorySet.sets.concat([newCategoryIndex]).sort(),
+    size:
+      // The size of the sets cannot be larger than either of the categories
+      Math.min(size, categorySet.size) *
+      // Randomize the size of the overlap
+      Math.random() *
+      // TODO: Figure out how often we want overlaps
+      // Math.round(Math.random()) basically says we only get overlaps half the time
+      Math.round(Math.random()),
+  })
+
+  // Now duplicate the existing pair sets
+  const pairSets = baseCategories.map(addToSet)
+  // Now duplicate the existing multi sets
+  const multiSets = categorySets.map(addToSet)
+
+  // Finally add the new category to the base categories
+  baseCategories.push(newCategory)
+  // And the new pari and multi sets
+  categorySets.push(...pairSets, ...multiSets)
+}
