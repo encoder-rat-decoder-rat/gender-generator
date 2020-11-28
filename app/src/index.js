a0e17e881a3ff639c173fd137385b71efbe75209
Dylan Box <dylanbox08@gmail.com>
2020-11-28 13:30:26 -0500

    Add: Facial Mesh and Color variance
    Adds a facial mesh to map points onto and a color contrast algorithm

--- app/src/index.js
+++ app/src/index.js
@@ -8,42 +8,24 @@ import {
   Point,
   Ticker,
   settings,
+  utils,
 } from "pixi.js";
 import * as PIXI from "pixi.js";
 import { DotFilter } from "@pixi/filter-dot";
 import { MultiColorReplaceFilter } from "@pixi/filter-multi-color-replace";
+
+import { UV_COORDS } from "@tensorflow-models/face-landmarks-detection/dist/mediapipe-facemesh/uv_coords.js";
+import { MESH_ANNOTATIONS } from "@tensorflow-models/face-landmarks-detection/dist/mediapipe-facemesh/keypoints.js";
+
 import seedrandom from "seedrandom";
 
 import GooeyFilter from "./GooeyFilter.js";
+import { contrast } from "./utils.js";
 
 import { downloadAsPNG } from "./downloadFrame.js";
 
 window.PIXI = PIXI;
 settings.FILTER_RESOLUTION = 2;
-
-const BG_COLOR = 0xadefd1;
-const FG_COLOR = 0x00203f;
-
-const FACE_LOCATIONS = [
-  // Hair
-  new Point(0, -350),
-  // Nose
-  new Point(0, 100),
-  // Lips
-  new Point(0, 250),
-  // Left Eye
-  new Point(-75, -100),
-  // Left Eyebrow
-  new Point(-75, -150),
-  // Left Ear
-  new Point(-300, 0),
-  // Right Eye
-  new Point(75, -100),
-  // Right Eyebrow
-  new Point(75, -150),
-  // Right Ear
-  new Point(300, 0),
-];
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
@@ -62,13 +44,27 @@ const queryParams = new URLSearchParams(window.location.search);
 const seed = queryParams.get("seed");
 const seededRandom = seedrandom(seed);
 
+const bgColorArray = [seededRandom(), seededRandom(), seededRandom()];
+// Ensure the FG color has at least a contrast ratio of 4.5: 1 for legibility
+// https://www.w3.org/TR/WCAG20-TECHS/G18.html
+let isContrastRatioAcceptable = false;
+let fgColorArray;
+while (!isContrastRatioAcceptable) {
+  fgColorArray = [seededRandom(), seededRandom(), seededRandom()];
+
+  isContrastRatioAcceptable = contrast(bgColorArray, fgColorArray) >= 4.5;
+}
+
+const bgColor = utils.rgb2hex(bgColorArray);
+const fgColor = utils.rgb2hex(fgColorArray);
+
 // Setup the pixi application
 const app = new Application({
   width: window.innerWidth,
   height: window.innerHeight,
   antialias: true,
   resizeTo: window,
-  backgroundColor: BG_COLOR,
+  backgroundColor: bgColor,
   sharedTicker: true,
   sharedLoader: true,
 });
@@ -88,8 +84,8 @@ const gooeyFilter = new GooeyFilter();
 const dotFilter = new DotFilter(1.05, 0);
 const colorReplace = new MultiColorReplaceFilter(
   [
-    [0xffffff, BG_COLOR],
-    [0x000000, FG_COLOR],
+    [0xffffff, bgColor],
+    [0x000000, fgColor],
   ],
   0.1
 );
@@ -100,16 +96,13 @@ app.stage.filters = [blurFilter, gooeyFilter, dotFilter, colorReplace];
 const faceContainer = new Container();
 app.stage.addChild(faceContainer);
 faceContainer.position.set(app.renderer.width, app.renderer.height);
-faceContainer.pivot.set(app.renderer.width / 2, app.renderer.height / 2);
-
-// Draw the oval behind the face
-const circle = new Graphics()
-  .lineStyle(3, 0xcccccc)
-  .drawEllipse(0, 0, 300, 400);
-circle.position.set(0, 0);
-circle.scale.set(0, 0);
-circle.destination = circle.position;
-faceContainer.addChild(circle);
+// Zoom in slightly
+faceContainer.scale.set(1.3, 1.3);
+// Center it
+faceContainer.pivot.set(
+  app.renderer.width / 2 / faceContainer.scale.x,
+  app.renderer.height / 2 / faceContainer.scale.y
+);
 
 // Set an interval loop to check if the ?frame query param is added to the url
 let currentParams = null;
@@ -123,25 +116,38 @@ const checkFrame = () => {
 window.setInterval(checkFrame, 2000);
 
 /**
- * Add a random circle
+ * Add a point on the face mesh
  *
  * @param      {Number}  [size=seededRandom()*14+8]  The size
  */
-function addOne(index) {
-  const size = seededRandom() * 75 + 10;
+function addOne(point, size = 4.5, color = 0xffffff) {
+  // The UV coords are normalized in terms of 0 -> 1 of the container
+  const [xRel, yRel] = point;
   const circle = new Graphics()
     .beginFill(0xffffff * seededRandom())
     .drawCircle(0, 0, size)
     .endFill();
-  circle.position.set(0, 0);
+  // TODO: reposition on window resize
+  circle.position.set(
+    xRel * app.view.width - app.view.width / 2,
+    yRel * app.view.height - app.view.height / 2
+  );
   circle.scale.set(0, 0);
-  circle.destination = FACE_LOCATIONS[index];
   faceContainer.addChild(circle);
 }
 
-// Add a new blob every 2.5 seconds up to 9
-for (let i = 0; i < 9; i += 1) {
-  window.setTimeout(addOne.bind(null, i), 5000 * i);
+// Add every point
+UV_COORDS.forEach((point) => {
+  addOne(point);
+});
+
+// Add all of the important points
+for (const key in MESH_ANNOTATIONS) {
+  if (!key.includes("Iris") && !key.includes("silhouette")) {
+    MESH_ANNOTATIONS[key].forEach((pointIndex) => {
+      addOne(UV_COORDS[pointIndex], seededRandom() * 20);
+    });
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -153,12 +159,6 @@ app.ticker.add((delta) => {
     child.scale.set(
       child.scale.x + (1 - child.scale.x) * 0.1,
       child.scale.y + (1 - child.scale.y) * 0.1
-    );
-
-    // Movement towards destination
-    child.position.set(
-      child.position.x + (child.destination.x - child.position.x) * 0.1,
-      child.position.y + (child.destination.y - child.position.y) * 0.1
     );
   });
 });
